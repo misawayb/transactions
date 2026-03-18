2ce7f3f0f3373e3ea3cb492d7bbcb66525a565c9
yuri <yuri@yurinoMacBook-Air.local>
2026-03-18 10:44:26 +0900

    add:categories tableにtype feat:Seeder

--- src/database/migrations/2026_03_17_051710_create_categories_table.php
+++ src/database/migrations/2026_03_17_051710_create_categories_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->enum('type', ['支出', '収入']);
             $table->string('name');
             $table->timestamps();
         });
---
+++ src/database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            ['name' =>'給与', 'type' => '収入'],
+            ['name' => '賞与', 'type' => '収入'],
+            ['name' => '食費', 'type' => '支出'],
+            ['name' => '住宅', 'type' => '支出'],
+            ['name' => '光熱費', 'type' => '支出'],
+        ];
+
+        foreach ($categories as $category){
+            Category::create($category);
+        }
+    }
+}
